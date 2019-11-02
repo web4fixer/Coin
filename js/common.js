55dabec7c6ef97162b461f5153fe498d7a868e9b
web4fixer <web4fixer@gmail.com>
2019-11-02 01:45:02 +0100

    first commit

---
+++ js/common.js
@@ -0,0 +1,103 @@
+$(function(){
+
+    $('.slider__box').slick({
+        dots:true,
+        infinite: true,
+        speed: 1000,
+        // fade:true,
+        cssEase: 'ease-in-out',
+        autoplaySpeed: 1000,
+        arrows: false,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        autoplay: true,
+        lazyLoad: 'ondemand',
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        swipeToSlide:true
+    });
+
+    $('.solve__inner').slick({
+        dots:true,
+        dotsClass:'dots__list',
+        infinite: true,
+        appendDots: $(".swiper__inner"),
+        speed: 2000,
+        autoplaySpeed: 2000,
+        arrows: false,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        autoplay: true,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        swipeToSlide:true
+    });
+
+    //        for footer accordion
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+
+    new WOW().init();
+
+
+
+    $('.counter').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+
+        $({ countNum: $this.text()}).animate({
+                countNum: countTo
+            },
+
+            {
+
+                duration: 8000,
+                easing:'linear',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                    //alert('finished');
+                }
+
+            });
+
+
+
+    });
+
+
+});
+$('.swiper__list').addClass('dots__list');
+
+$(function () {
+    $('.about__inner').slick({
+        dots:true,
+        dotsClass:'dots__list',
+        infinite: true,
+        appendDots: $(".steps__inner"),
+        speed: 2000,
+        autoplaySpeed: 2000,
+        arrows: false,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        autoplay: true,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        swipeToSlide:true
+    });
+});
+$('.steps__inner').addClass('.dots__list');
